5dea86204e7d5d721fd60802efac40cdf898a439
rajacsp <raja.csp@gmail.com>
2019-02-06 16:17:23 -0500

    Jason, map, html integrated

--- Main.py
+++ Main.py
@@ -3,36 +3,86 @@ import os
 import sys
 from flask import request
 from random import randint
+import json
+from pprint import pprint
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('index.html')
 
-@app.route('/here-is-your-car', methods=['POST'])
-def result():
-    
-    car_brand  = request.form.get('car_brand')
-    
-    # You can validate the car brands. If someone is telling the wrong brand name, reply them with the wrong answer
-    
-    address = get_parked_place()
-    
-    user = {
-        'car_brand' : car_brand,
-        'address': address,
-        'ticket_amount' : ticket_amount 
+'''
+    http://localhost:5000/country_code?country=india
+'''
+@app.route('/country_code')
+def find_country_code():
+
+    country  = request.args.get('country')
+
+    code = get_country_code(country)
+
+    print('code : ', code)
+
+    result = {
+        'country' : country,
+        'code': code
     }
     
     #return content
-    return render_template('result.html', user=user)
+    return render_template('country-code.html', result=result)
 
-def get_parked_place():
-    return '288, Spadina Road'
+'''
+    http://localhost:5000/show/map
+'''
+@app.route("/show/map" , methods=['POST'])
+def show_map():
 
-def get_ticket_amount():
-    return 45
+    country1  = request.form.get('country1')
+    country2  = request.form.get('country2')
+
+    code1 = get_country_code(country1)
+    code2 = get_country_code(country2)
+
+    print('code1 : ', code1)
+
+    result = {
+        'country1' : country1,
+        'code1': code1,
+        'country2' : country2,
+        'code2': code2
+    }
+    
+    #return content
+    return render_template('country-code.html', result=result)    
+
+
+@app.route('/samplemap')
+def get_sample_map():
+    return render_template('sample-map.html')    
+
+
+def get_lat_long(country_code):
+
+    with open('assets/countrycode-latlong-array.json') as f:
+        data = json.load(f)
+
+    return data[country_code]    
+
+def get_country_code(country):
+
+    with open('assets/countries.json') as f:
+        data = json.load(f)
+
+    #print(data['country_codes'])
+
+    codes = data['country_codes']
+
+    for item in codes:
+        #print(item)
+
+        if(item['name'].lower() == country):
+            return item['code']
 
 if __name__ == '__main__':
     host = os.environ.get('IP', '127.0.0.1')
---
+++ json-reader.py
@@ -0,0 +1,44 @@
+import json
+from pprint import pprint
+
+
+def test():
+    with open('assets/countrycode-latlong-array.json') as f:
+        data = json.load(f)
+
+    pprint(data)
+
+def get_lat_long(country_code):
+
+    with open('assets/countrycode-latlong-array.json') as f:
+        data = json.load(f)
+
+    return data[country_code]
+
+def get_country_code(country):
+
+    with open('assets/countries.json') as f:
+        data = json.load(f)
+
+    #print(data['country_codes'])
+
+    codes = data['country_codes']
+
+    for item in codes:
+        #print(item)
+
+        if(item['name'].lower() == country):
+            return item['code']
+
+    #return codes[country_code]    
+
+#test()
+
+#
+#print(lat_long)
+
+#country_code = get_country_code('Canada')
+country_code = get_country_code('india')
+#print(country_code)
+lat_long = get_lat_long('ca')
+print(lat_long)
